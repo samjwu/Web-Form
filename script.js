abe28541e9288bffbba03616daefbb0cbd7eee35
samjwu <swm1000@hotmail.com>
2017-11-17 22:27:50 -0700

    add form validation

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
-$(function() {
+$(document).ready(function() {
     $("#birthday").datepicker();
+	$('#birthday').datepicker("option", "onSelect", function(dateText, inst) {
+    var birthDate = $(this).datepicker('getDate');
+    var userAge = getAge(birthDate);
+	if (userAge < 18) {
+        alert("Sorry, you may not register.");
+    }
+	});
 });
+
+function getAge(birthDate) {
+    var currentDate = new Date();
+    var userAge = currentDate.getFullYear() - birthDate.getFullYear();
+    var month = currentDate.getMonth() - birthDate.getMonth();
+	var day = currentDate.getDate() - birthDate.getDate();
+    if ((month < 0) || (month == 0 && day < 0)) {
+        userAge--;
+    }
+    return userAge;
+}
+
+//var userAge = parseInt(prompt("What's your age?"),10);
+//if (isNaN(userAge)) {
+	//console.log("No input was entered.");
+//} else if (userAge < 13) {
+	//console.log("Sorry, may not register.");
+//} else {
+	//console.log("You may proceed to register.");
+//}
